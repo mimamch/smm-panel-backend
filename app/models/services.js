c1b784194dba2e5eff1631182ddf97cdce314890
mimamch <mimamch28@gmail.com>
2022-05-07 15:42:40 +0700

    adding timestamp to models

--- app/models/services.js
+++ app/models/services.js
@@ -1,24 +1,30 @@
 const { default: mongoose } = require("mongoose");
 
-const CategorySchema = new mongoose.Schema({
-  name: String,
-});
+const CategorySchema = new mongoose.Schema(
+  {
+    name: String,
+  },
+  { timestamps: true }
+);
 
 const Category = mongoose.model("Category", CategorySchema);
 
-const ServicesSchema = new mongoose.Schema({
-  serviceId: Number,
-  name: String,
-  type: String,
-  category: String,
-  categoryId: String,
-  rate: Number,
-  min: Number,
-  max: Number,
-  desc: String,
-  dripfeed: Boolean,
-  refill: Boolean,
-});
+const ServicesSchema = new mongoose.Schema(
+  {
+    serviceId: Number,
+    name: String,
+    type: String,
+    category: String,
+    categoryId: String,
+    rate: Number,
+    min: Number,
+    max: Number,
+    desc: String,
+    dripfeed: Boolean,
+    refill: Boolean,
+  },
+  { timestamps: true }
+);
 
 const Services = mongoose.model("Services", ServicesSchema);
 
--- app/models/user.js
+++ app/models/user.js
@@ -2,20 +2,30 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, "Username Harus Di Isi"],
-    unique: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username Harus Di Isi"],
+      unique: true,
+    },
+    fullName: { type: String, required: [true, "Nama Harus Di Isi"] },
+    balance: { type: Number, default: 0 },
+    email: {
+      type: String,
+      required: [true, "Email Harus Di Isi"],
+      unique: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Nomor Telepon Harus Di Isi"],
+    },
+    password: { type: String, required: [true, "Password Harus Di Isi"] },
+    totalDeposit: { type: Number, default: 0 },
+    balanceUsed: { type: Number, default: 0 },
   },
-  fullName: { type: String, required: [true, "Nama Harus Di Isi"] },
-  balance: { type: Number, default: 0 },
-  email: { type: String, required: [true, "Email Harus Di Isi"], unique: true },
-  phoneNumber: { type: String, required: [true, "Nomor Telepon Harus Di Isi"] },
-  password: { type: String, required: [true, "Password Harus Di Isi"] },
-  totalDeposit: { type: Number, default: 0 },
-  balanceUsed: { type: Number, default: 0 },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 
