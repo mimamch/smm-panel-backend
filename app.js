3030de1416c6d75cf71ef5b07a2777d574048784
Muhammad Imam Choirudin <mimamch28@gmail.com>
2022-05-16 19:14:11 +0700

    disable logs for production

--- app.js
+++ app.js
@@ -29,7 +29,9 @@ app.use(function (req, res, next) {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(logger("dev"));
+if (process.env.NODE_ENV == "development") {
+  app.use(logger("dev"));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
